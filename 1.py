4012da4a2c8d5d4e1eade14ef2e1fde9937c73ea
Oskar Skrzypiński <92017887+Bambvsini@users.noreply.github.com>
2022-02-23 15:10:37 +0100

    update

--- 1.py
+++ 1.py
@@ -1,4 +1,42 @@
 import sys
+import math
+
+a = 'inżynieria systemów informatycznych'
+print(type(a), "\n", a)
 
 
-print(sys.version[::-1])
+b = 5
+print(type(b), "\n", b)
+
+
+c = 5.1
+print(type(c), "\n", c)
+
+
+d = 2+3j
+print(type(d), "\n", d)
+
+
+# nowa_zmienna
+# del a
+# print(type(a), "\n", a)
+
+f = 'isi'
+g = 'grupa 3'
+
+print(f+' '+g)
+
+h = 7
+i = 2
+print(h // i)
+print(2/4 ** i)
+print(math.pow(2/4, i))
+
+h += 1
+print(h)
+
+a = 5
+b = 3
+c = a - b
+print("wynik działania %(z1)d - %(z2)d = %(z3)d" %{'z1':a, 'z2':b, 'z3':c})
+print("wynik działania {0:d} - {1:d} = {2:d}".format(a, b, c))
